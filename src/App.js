a751fd38192720911c6f8e5984615f49aaa1a871
Dan <70431670+Danielx2003@users.noreply.github.com>
2024-02-20 20:33:42 +0000

    Added Main page placeholder

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Splash from "./pages/Splash.jsx"
 import Login from "./pages/Login.jsx"
+import Main from "./pages/Main.jsx"
 import Nav from "./components/Nav.jsx"
 
 function App() {
@@ -18,6 +19,10 @@ function App() {
           <Route
             path="/login"
             element={<Login />}
+          />
+          <Route
+            path="/main"
+            element={<Main />}
           />
         </Routes>
       </BrowserRouter>
